44bdcb6c3f0adc7bbe180b11b8ed5494c39cc78a
aldubar <aldubar@student.42.fr>
2022-01-20 21:27:21 +0100

    form to search in progress

--- pages/index.js
+++ pages/index.js
@@ -11,17 +11,52 @@ export async function getStaticProps() {
     }
   }
 }
+
+function Form() {
+  const registerUser = async event => {
+    event.preventDefault()
+
+    const res = await fetch(
+      'https://www.youtube.com/results?search_query=SEARCH',
+      {
+        body: JSON.stringify({
+          name: event.target.name.value
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: 'POST'
+      }
+    )
+
+    const result = await res.json()
+  }
+
+  return (
+    <form onSubmit={registerUser}>
+      <label htmlFor="yt_seqrch">Name</label>
+      <input id="yt search" name="yt_search" type="text" autoComplete="yt_search" required />
+      <button type="submit">Register</button>
+    </form>
+  )
+}
+
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
+      <section className={utilStyles.title}>
         <p>Welcome to yt2mp3 Downloader</p>
-      </section>
+	  </section>
+
+	  <section>
+	  	<p>Search</p>
+	  </section>
+
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <h2 className={utilStyles.headingLg}>Results:</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
