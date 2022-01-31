fbc1624b20f9a1c565cde4f5c0da15968cb845f2
aldubar <aldubar@student.42.fr>
2022-01-31 20:25:15 +0100

    v2 done

--- app/pages/index.js
+++ app/pages/index.js
@@ -2,7 +2,10 @@ import Head from 'next/head'
 import { useState, useEffect } from "react";
 import Layout, { siteTitle } from '../components/layout'
 import { dirListing } from '../lib/dirListing'
-
+import { FaPhotoVideo } from 'react-icons/fa';
+import { MdSlowMotionVideo } from 'react-icons/md';
+import { RiVideoDownloadLine } from 'react-icons/ri';
+import { BiReset } from 'react-icons/bi';
 
 const NoSsr = ({ children }) => {
 	const [mountedState, setMountedState] = useState(false);
@@ -21,17 +24,16 @@ export async function getServerSideProps() {
   }
 }
 
-function Form( {actualList, setActualList} ) {
+function Form( {actualList, setActualList, conversionState, setConversionState} ) {
 
 	const [input, setInput] = useState({});
 	const [dl_url, setURL] = useState();
 	const [title, setTitle] = useState();
 	const [quality, setQuality] = useState();
-	const [conversionState, setConversionState] = useState('waiting');
 	const [inputState, setInputState] = useState('waiting');
 
 	const qualityChange = (event) => {
-		const name = event.target.name;
+	//	const name = event.target.name;
 		const value = event.target.value;
 		setQuality(value.substring(0,3));
 	}
@@ -98,7 +100,7 @@ function Form( {actualList, setActualList} ) {
 			});
 
 			const resList = await reqList.json();
-			setActualList([])
+			setActualList([]);
 			for (let i = 0; i < resList.length; i++) {
 				setActualList([...actualList, {"id":resList[i] , "dl_path": "yt/" + resList[i], "filename": resList[i], "thumbPath": "thumb/" + resList[i].replace('.mp3', '.jpeg')}])
 			}	
@@ -107,32 +109,32 @@ function Form( {actualList, setActualList} ) {
 	if (conversionState === 'found') {
 		p = 'p-5 text-white animate-pulse';
 		msg = 'Found! Converting now...';
-		input_form = 'text-center w-full md:max-w-[50%] bg-green-500 text-gray-500 pointer-events-none';
+		input_form = 'text-center w-full md:max-w-[50%] bg-green-500 text-gray-500 pointer-events-none h-12';
 	}
 
 	if (conversionState === 'waiting' && inputState === 'waiting') {
 		p = 'p-5 text-white';
 		msg = 'waiting for conversion...';
-		input_form = 'w-full md:max-w-[50%] text-center';
+		input_form = 'w-full md:max-w-[50%] text-center h-12';
 	}
 
 	if (conversionState === 'waiting' && inputState === 'valid') {
 		p = 'invisible';
 		button = 'text-xl text-white bg-red-500 rounded-xl w-1/3 hover:text-red-500 hover:bg-white hover:w-1/2 ease-out duration-200';
-		input_form = 'bg-green-500 w-full md:max-w-[50%] text-center';
+		input_form = 'bg-green-500 w-full md:max-w-[50%] text-center h-12';
 		quality_btn = 'text-color-white bg-black m-10 px-5 border rounded';
 	}
 
 	if (conversionState === 'waiting' && inputState === 'invalid') {
 		p = 'p-5 text-red-500';
 		msg = 'Invalid URL';
-		input_form = 'bg-red-500 w-full md:max-w-[50%] text-center';
+		input_form = 'bg-red-500 w-full md:max-w-[50%] text-center h-12';
 	}
 
 	if (conversionState === 'conversion error') {
 		p = 'p-5 text-red-500 text-2xl';
 		msg = 'Conversion error';
-		input_form = 'text-black w-full md:max-w-[50%] text-center';
+		input_form = 'text-black w-full md:max-w-[50%] text-center h-12';
 	}
 
 	if (conversionState === 'converted')  {
@@ -145,7 +147,7 @@ function Form( {actualList, setActualList} ) {
 	if (conversionState === 'loading') {
 		p = 'p-5 text-white';
 		msg = 'Loading... Please wait a few seconds...';
-		input_form = 'text-black bg-gray-500 w-full md:max-w-[50%] text-center pointer-events-none';
+		input_form = 'text-black bg-gray-500 w-full md:max-w-[50%] text-center pointer-events-none h-12';
 	}
 
 	return (
@@ -160,10 +162,10 @@ function Form( {actualList, setActualList} ) {
 				<option>192 Kbps</option>
 			  	<option>128 Kbps</option>
 		  </select>
+		  { (conversionState === 'converted' || conversionState === 'conversion error') ? <div className="grid justify-items-center"><a href="/"><BiReset className="text-7xl animate-spin"/></a></div> : <></> }
 	      <br/>
 		  <button className={button} type="submit">Convert</button><br/>
 		  <a className={dl_link} href={dl_url}>{title}</a>
-		  { (conversionState === 'converted') ? <div className="p-10 underline text-gray-500"><a href="/">Reset</a></div> : <></> }
 	    </form>
 		</>
 	)
@@ -172,27 +174,35 @@ function Form( {actualList, setActualList} ) {
 export default function Home({ filesList }) {
 
   const [actualList, setActualList] = useState(filesList);
+  const [conversionState, setConversionState] = useState('waiting');
   
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className="text-red-600 text-xl text-center p-10">
-        <h2>Welcome to yt2mp3 Downloader</h2>
+      <section className="text-red-600 text-xl text-center p-5">
+		  <div className="text-6xl p-5 grid grid-cols-3 justify-items-center">
+		  { (conversionState === 'loading' || conversionState === 'found') ? 	<>
+													  <div className="invisible md:visible"><FaPhotoVideo /></div>
+													  <div className="animate-spin"><MdSlowMotionVideo /></div>
+													  <div className="invisible md:visible "><RiVideoDownloadLine /></div>
+												</> : <><div className="invisible md:visible hover:animate-ping"><FaPhotoVideo /></div>
+													  <div className="hover:animate-spin"><MdSlowMotionVideo /></div>
+													  <div className="invisible md:visible  hover:animate-ping"><RiVideoDownloadLine /></div>
+												</> }
+		</div>
 	  </section>
 	  <NoSsr>
 	  <section>
-		<Form actualList={actualList} setActualList={setActualList}/>
+		<Form actualList={actualList} setActualList={setActualList} conversionState={conversionState} setConversionState={setConversionState}/>
 	  </section>
 	  </NoSsr>
-      <section>
-	    <hr className="p-2 self-stretch border-gray-600 border-1" />
-     	<h3 className="text-orange-500 p-5 text-center text-xl">LINKS AVAILABLE:</h3>
-        	<ul className="place-content-center place-items-stretch grid md:grid-cols-2 lg:flex gap-5 text-center p-10">
+      <section>	
+	    	<ul className="place-content-center place-items-stretch align-items-stretch grid md:grid-cols-2 lg:flex gap-10 text-center p-10">
           	{actualList.map(({ id, dlPath, filename, thumbPath }) => (
-            	<li className="justity-items-center p-5 border rounded-lg  border-orange-500 hover:border-black hover:bg-orange-500" key={id}>
-				<a className="text-orange-500 hover:text-black hover:animate-pulse hover:underline" href={dlPath}>
+            	<li className="text-black hover:text-orange-500 justity-items-center p-5 bg-orange-500 border rounded-lg  border-white hover:border-orange-500 hover:bg-black transition transform hover:-translate-y-2 " key={id}>
+				<a className="hover:underline" href={dlPath}>
 					<img className="justify-content-center" src={thumbPath} height="100%" width="100%"/>
 					<p className="pt-5">{filename}</p>
 				</a>
